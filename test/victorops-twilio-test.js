2192539a4a94877caf82e3fb6bc3c7dd10835695
Dave <dscott@victorops.com>
2017-07-02 19:44:21 -0600

    clean up

--- test/victorops-twilio-test.js
+++ test/victorops-twilio-test.js
@@ -1,18 +1,15 @@
 const chai = require('chai');
 const mocha = require('mocha');
 const app = require('../victorops-twilio');
-
-console.log(app.callOrMessage);
+const assert = require('assert');
 
 const twiml = {gather: function() {return twiml}, say: function() {}};
 const voice = {voice: 'my voice'};
 const callerId = 'alsjdflkajsdflkjasdlf';
 
-const assert = require('assert');
-
 describe('callOrMessage()', function() {
   const p = app.callOrMessage(voice, twiml);
-  it(`should be true`, function() {
+  it(`should be a function`, function() {
     assert.equal(typeof app.callOrMessage === 'function', true, typeof app.callOrMessage);
   });
   it(`should return a promise`, function() {
